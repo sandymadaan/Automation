d9e79c022bc12b18ca5ada432fd6524ec2644847
sandy <sandy@kaur-sandeep.(none)>
2013-03-31 11:44:04 +0530

    tcc removed

--- tcc/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.template import RequestContext
-
-def base_template(request):
-    if not request.user.is_active:
-        return {"base_template": "base_noclient.html"}
-    if not request.user.is_staff:
-        return {"base_template": "base_client.html"}
-    return "base.html"
