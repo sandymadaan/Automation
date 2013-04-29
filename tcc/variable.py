d577ab907df302687fc4712a202424b640e524ae
sandy <sandy@kaur-sandeep.(none)>
2013-04-30 00:53:44 +0530

    tcc added

---
+++ tcc/variable.py
@@ -0,0 +1,23 @@
+"""
+%% variable.py %%
+
+This file is a static file that assign some constant values to the variables.
+"""
+
+from Automation.tcc.views import *
+collegeincome = 15
+admincharge = 5
+workcharge = 2
+routine_ratio1 = 30
+routine_ratio2 = 70
+servicetax = 0.12
+servicetaxprint = 12.0
+educationtax = 0.0024
+educationtaxprint =0.24
+highereducationtax = 0.0012
+highereducationtaxprint =0.12
+individual_ratio1 = 60
+individual_ratio2 = 40
+institutional_ratio1 = 40
+institutional_ratio2 = 60
+
